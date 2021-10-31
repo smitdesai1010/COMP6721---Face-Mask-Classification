fbe8503f8a38692dbe8de321f86a25efb2ab0dd5
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2021-10-31 08:44:43 -0400

    added preprocessing

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 # COMP6721---Face-Mask-Classification
 
-#### Medium links
+## Medium links
 
-##### What are CNNs
+#### What are CNNs
 https://medium.com/nybles/a-brief-guide-to-convolutional-neural-network-cnn-642f47e88ed4
 https://prvnk10.medium.com/cnn-architectures-ecefaa2359ff
 
 
-###### How to improve it's architecture
+#### How to improve CNN architecture
 https://medium.com/@dipti.rohan.pawar/improving-performance-of-convolutional-neural-network-2ecfe0207de7
 
 
-##### Pytorch and CNNs
+#### Pytorch and CNNs
 https://medium.com/thecyphy/train-cnn-model-with-pytorch-21dafb918f48
+
+#### Installing pytorch
+```
+pip3 install torch==1.10.0+cu102 torchvision==0.11.1+cu102 torchaudio===0.10.+cu102 -f https://download.pytorch.org/whl/cu102/torch_stable.html
+```
+
+#### Preprocessing using torchvision
 https://medium.com/secure-and-private-ai-writing-challenge/loading-image-using-pytorch-c2e2dcce6ef2
-
-
-#### Recreate virtual env
-https://stackoverflow.com/questions/7438681/how-to-duplicate-virtualenv
+https://pytorch.org/vision/stable/transforms.html
--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-import torch
+import os
+import matplotlib.pyplot as plt
+import torch, torchvision
+from torchvision import transforms
+
+DATASET_DIR = os.path.dirname(os.path.abspath(__file__)) + './dataset'
+IMG_SIZE = 255
+BATCH_SIZE = 32
+SHUFFLE = True
+
+#Applying Transformation
+transforms = transforms.Compose([
+    transforms.Resize([IMG_SIZE,IMG_SIZE]), #resizing every image into the desired values
+    transforms.RandomHorizontalFlip(), #Flips images horizontally with a probability of 0.5
+    transforms.ToTensor()])     #size normalization and conversation to tensor
+
+#Loads the images and labels from the specified folder and applies the given transformation
+data = torchvision.datasets.ImageFolder(DATASET_DIR, transform=transforms)                                       
+print(data)
+
+#Loading data into a generator which provides images in a batch 
+dataloader = torch.utils.data.DataLoader(data, batch_size=BATCH_SIZE, shuffle=SHUFFLE)
+
+#Testing
+images, labels = next(iter(dataloader))
+plt.imshow(images[0].permute(1, 2, 0), label = labels[0])
+plt.show()
